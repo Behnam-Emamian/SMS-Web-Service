8ca61a375eb169e70213cdff442436f89a8dd811
Behnam Emamian <emamian@persianprocess.com>
2019-10-23 15:06:41 +1100

    Add check permision

--- Solution/SmsWebService/MainPage.xaml.cs
+++ Solution/SmsWebService/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Xamarin.Forms;
+﻿using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace SmsWebService
 {
@@ -7,6 +10,23 @@ namespace SmsWebService
         public MainPage()
         {
             InitializeComponent();
+
+            Task.Factory.StartNew(async () =>
+            {
+                var smsPermissionStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Sms);
+                if (smsPermissionStatus != PermissionStatus.Granted)
+                {
+                    var smsPermissionRequestResult = await CrossPermissions.Current.RequestPermissionsAsync(Permission.Sms);
+                    if (smsPermissionRequestResult[Permission.Sms] != PermissionStatus.Granted)
+                    {
+                        Device.BeginInvokeOnMainThread(async () =>
+                        {
+                            if (await DisplayAlert("SMS Permission", "Without SMS permission, the app is not working!!!", "Accept", "Cancel"))
+                                await CrossPermissions.Current.RequestPermissionsAsync(Permission.Sms);
+                        });
+                    }
+                }
+            })
         }
     }
 }
